02a689255f9341da8d7df8c645386d9eab7d9b63
Aklima-akter <aklima.aklm553@gmail.com>
2021-09-04 14:36:08 +0600

    all finished

--- js/api.js
+++ js/api.js
@@ -5,21 +5,45 @@ const cityName = document.getElementById('city-name');
 const tempature = document.getElementById('tempature');
 const weatherSiuation = document.getElementById('weather-situation');
 const weathericon = document.getElementById('weather-icon');
+const weatherStutas = document.querySelector('.weather-status');
 
 
 
-searchButton.addEventListener('click', () => {
-    const searchValue = inputField.value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}&units=metric`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            cityName.innerText = `${data.name}`;
-            tempature.innerText = `${data.main.temp} C`;
-            weatherSiuation.innerText = `${data.weather[0].main}`
-            weathericon.innerHTML = `
-            <img  src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-            `
-        })
+inputField.addEventListener('keyup', (e) => {
+
+    if (e.keyCode == 13) {
+
+        const searchValue = inputField.value;
+        inputField.value = ''
+        if (searchValue === '') {
+            weatherStutas.innerHTML = ''
+            weatherStutas.innerText = 'Please search with city!';
+            return;
+        }
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}&units=metric`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.cod == 404) {
+                    weatherStutas.innerText = 'Invalid city Name!';
+                    return;
+                }
+                weatherStutas.innerHTML = `
+                <p id="weather-icon">
+                <img  src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+                </p>
+                <h1 id="city-name">${data.name}</h1>
+                <h3><span id="tempature">${data.main.temp} &deg;C</span></h3>
+                <h1 id="weather-situation" class="lead">${data.weather[0].main}</h1>
+                    
+                `
+
+
+            })
+    }
+
 })
+/* 
+ 
+ */
